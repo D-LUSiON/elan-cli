86fce48e4f2c6645c8316d0e334a2c7546e8863e
Lubomir Peikov <l.peikov@webfashion.bg>
2019-11-01 19:09:43 +0200

    update: Now serves with the project's Electron

--- commands/serve.js
+++ commands/serve.js
@@ -3,8 +3,11 @@ const {
 } = require('child_process');
 const nodemon = require('nodemon');
 const path = require('path');
+const fs = require('fs-extra');
 const chalk = require('chalk');
 const ng = path.join(process.cwd(), 'node_modules', '@angular', 'cli', 'bin', 'ng');
+
+const readline = require('readline');
 
 class Serve {
     constructor(args) {
@@ -52,32 +55,62 @@ class Serve {
 
     electronWatch() {
         return new Promise((resolve, reject) => {
-            console.log(chalk.cyan('INFO'), 'Type "rs" to restart application');
-            console.log(chalk.cyan('INFO'), `Press Ctrl-C to quit\n`);
+            let electron_version = '';
+            let electron_local = true;
+            let electron_path;
+            if (fs.existsSync(path.join(process.cwd(), 'node_modules', 'electron'))) {
+                electron_local = true;
+                electron_path = path.resolve(process.cwd(), 'node_modules', 'electron', 'dist', 'electron');
+                electron_version = require(path.resolve(process.cwd(), 'node_modules', 'electron', 'package.json')).version;
+            } else {
+                electron_local = false;
+                electron_path = path.resolve(__dirname, '..', 'node_modules', 'electron', 'dist', 'electron');
+                electron_version = require(path.resolve(__dirname, '..', 'node_modules', 'electron', 'package.json')).version;
+            }
 
             nodemon({
-                exec: 'electron ./electron',
+                exec: `${electron_path} ./electron`,
                 verbose: true,
                 watch: [
                     './www',
-                    './electron'
+                    './electron',
+                    './resources'
                 ],
                 ext: '*',
+                env: {
+                    'NODE_ENV': 'development'
+                },
                 delay: 2.5,
                 signal: 'SIGHUP',
             });
 
-            nodemon.on('start', () => {
-                console.log(chalk.cyan('INFO'), 'App has started');
-            }).on('quit', () => {
-                console.log(chalk.cyan('INFO'), 'App has quit');
-                resolve();
-            }).on('error', () => {
-                console.log(chalk.red('ERROR'), 'Error occured!');
-                reject('Error occured while running monitoring!');
-            }).on('restart', (files) => {
-                console.log(chalk.cyan('INFO'), 'App restarted', files instanceof Array ? `due to: ${chalk.green(files.join())}` : '');
-            });
+            let first_start = true;
+
+            nodemon
+                .on('start', () => {
+                    if (first_start) {
+                        console.log(chalk.cyan('INFO'), 'App has started');
+                        first_start = false;
+                    }
+                })
+                .on('restart', (files) => {
+                    console.log(chalk.cyan('INFO'), 'App restarted', files instanceof Array ? `due to: ${chalk.green(files.join())}` : '');
+                })
+                .on('quit', () => {
+                    console.log(chalk.cyan('INFO'), 'App has quit');
+                    resolve();
+                })
+                .on('error', () => {
+                    console.log(chalk.red('ERROR'), 'Error occured!');
+                    reject('Error occured while running monitoring!');
+                });
+
+            console.log(`\n`);
+            console.log(chalk.cyan('INFO'), `Serving project using ${electron_local ? 'your project': 'ElAn'}'s Electron v${electron_version}`);
+            console.log(chalk.cyan('INFO'), 'Type "rs" to restart application');
+            console.log(chalk.cyan('INFO'), `Press Ctrl-C to quit`);
+            console.log(`\n`);
+
         });
     }
 }
